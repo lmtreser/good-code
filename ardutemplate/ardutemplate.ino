67cbb7f6556f95bb6332e531826bffe9b87e5e2f
lmtreser <lmtreser@gmail.com>
2024-02-06 16:56:09 -0300

    add arduino firmware template

---
+++ ardutemplate/ardutemplate.ino
@@ -0,0 +1,41 @@
+/*
+ *  TITULO DEL PROYECTO
+ *
+ *  Breve descripción del proyecto
+ *
+ *  Sobre el circuito: se recomienda enumerar los componentes conectados a cada 
+ *  entrada y a cada salida.
+ *
+ *  Bibliotecas de terceros: se recomienda adjuntar el código o url de cada biblioteca
+ *  de terceros utilizadas en el proyecto.
+ *
+ *  Creado dia mes año
+ *  Nombre del autor
+ *  Modificado dia mes año
+ *  Nombre del autor
+ *
+ *  https://url/of/online/tutorial.cc
+ *
+*/
+
+#include "pinout.h"
+#include "credentials.h"
+
+// Instancia de objetos
+// .....
+
+// Variables globales
+// .....
+
+// Constantes
+// .....
+
+void setup() {
+
+
+}
+
+void loop() {
+
+
+}
