d92968f51ca8974f0abee7aa60b1cc6295bf0489
lmtreser <lmtreser@gmail.com>
2024-02-07 10:46:31 -0300

    add ESP8266/ESP32 WiFi Firmware Template

---
+++ wifi_template/wifi_template.ino
@@ -0,0 +1,46 @@
+/*
+ *  TITULO DEL PROYECTO
+ *
+ *  Breve descripción del proyecto
+ *
+ *  Sobre el circuito: se recomienda enumerar los componentes conectados a cada 
+ *  entrada y a cada salida.
+ *
+ *  Bibliotecas de terceros: se recomienda adjuntar el código o url de cada biblioteca
+ *  de terceros utilizadas en el proyecto.
+ *
+ *  Creado dia mes año
+ *  Nombre del autor
+ *  Modificado dia mes año
+ *  Nombre del autor
+ *
+ *  https://url/of/online/tutorial.cc
+ *
+*/
+
+#include "pinout.h"
+#include "credentials.h"
+#ifdef ESP8266
+#include <ESP8266WiFi.h>
+#elif defined(ESP32)
+#include <WiFi.h>
+#endif
+
+// Instancia de objetos
+// .....
+
+// Variables globales
+// .....
+
+// Constantes
+// .....
+
+void setup() {
+
+
+}
+
+void loop() {
+
+
+}
